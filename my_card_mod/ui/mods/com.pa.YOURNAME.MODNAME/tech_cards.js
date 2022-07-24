05586e57ee9d404d62609e3498ea38f2b78e1a49
Quitch <Quitch@users.noreply.github.com>
2022-07-24 10:00:36 +0100

    Improve implementation and documentation

--- my_card_mod/ui/mods/com.pa.YOURNAME.MODNAME/tech_cards.js
+++ my_card_mod/ui/mods/com.pa.YOURNAME.MODNAME/tech_cards.js
@@ -1,17 +1,30 @@
 function addCards() {
   try {
+    // Add cards to deck
     if (!model.gwoDeck) model.gwoDeck = [];
+    // IDs match the card filename minus file extension e.g. "gwc_start_vehicles"
+    var cards = ["YOUR_CARD_ID_1", "YOUR_CARD_ID_2", "YOUR_CARD_ID_N"];
+    model.gwoDeck = model.gwoDeck.concat(cards);
+
+    // Setup cards for tooltips
     if (!model.gwoCardsToUnits) model.gwoCardsToUnits = [];
-    model.gwoDeck.push("YOUR_TECH_ID_1", "YOUR_TECH_ID_2");
-    model.gwoCardsToUnits.push({
-      id: "YOUR_TECH_ID_1",
-      // Use the base_commander for commander and always the use unit, not the ammo, etc.
-      units: ["AFFECTED_UNIT_PATH_1", "AFFECTED_UNIT_PATH_2", "AND_SO_ON"],
-    });
-    model.gwoCardsToUnits.push({
-      id: "YOUR_TECH_ID_2",
-      units: ["AFFECTED_UNIT_PATH_1", "AFFECTED_UNIT_PATH_2", "AND_SO_ON"],
-    });
+    var cardUnits = [
+      {
+        id: "YOUR_TECH_ID_1",
+        // Use the base_commander for commander and always the use unit, not the ammo, etc.
+        // example path: "/pa/units/land/assault_bot/assault_bot.json"
+        units: ["AFFECTED_UNIT_PATH_1", "AFFECTED_UNIT_PATH_2", "AND_SO_ON"],
+      },
+      {
+        id: "YOUR_TECH_ID_2",
+        units: ["AFFECTED_UNIT_PATH_1", "AFFECTED_UNIT_PATH_2", "AND_SO_ON"],
+      },
+      {
+        id: "YOUR_TECH_ID_N",
+        units: ["AFFECTED_UNIT_PATH_1", "AFFECTED_UNIT_PATH_2", "AND_SO_ON"],
+      },
+    ];
+    model.gwoCardsToUnits = model.gwoCardsToUnits.concat(cardUnits);
   } catch (e) {
     console.error(e);
     console.error(JSON.stringify(e));
