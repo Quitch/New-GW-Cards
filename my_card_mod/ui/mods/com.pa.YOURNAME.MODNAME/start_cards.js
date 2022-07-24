f48bb7d675e781c05764cbfc404132a29414c4f7
Quitch <Quitch@users.noreply.github.com>
2022-07-24 10:15:03 +0100

    Show start card addition to infinity

--- my_card_mod/ui/mods/com.pa.YOURNAME.MODNAME/start_cards.js
+++ my_card_mod/ui/mods/com.pa.YOURNAME.MODNAME/start_cards.js
@@ -5,7 +5,8 @@ function addCards() {
     // IDs match the card filename minus file extension e.g. "gwc_start_vehicles"
     model.gwoNewStartCards.push(
       { id: "YOUR_LOADOUT_ID_1" },
-      { id: "YOUR_LOADOUT_ID_2" }
+      { id: "YOUR_LOADOUT_ID_2" },
+      { id: "YOUR_LOADOUT_ID_N" }
     );
   } catch (e) {
     console.error(e);
