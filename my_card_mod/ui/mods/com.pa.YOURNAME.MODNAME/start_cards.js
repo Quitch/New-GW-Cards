d30971cdececeb17bb807e1b2ab39d633cfeba0b
Quitch <Quitch@users.noreply.github.com>
2022-07-24 12:50:25 +0100

    Support loading unlocked loadouts

--- my_card_mod/ui/mods/com.pa.YOURNAME.MODNAME/start_cards.js
+++ my_card_mod/ui/mods/com.pa.YOURNAME.MODNAME/start_cards.js
@@ -1,9 +1,18 @@
 function addCards() {
   try {
-    // Add loadouts
+    // Add locked loadouts
     if (!model.gwoNewStartCards) model.gwoNewStartCards = [];
     // IDs match the card filename minus file extension e.g. "gwc_start_vehicles"
     model.gwoNewStartCards.push(
+      { id: "YOUR_LOADOUT_ID_1" },
+      { id: "YOUR_LOADOUT_ID_2" },
+      { id: "YOUR_LOADOUT_ID_N" }
+    );
+
+    // Add unlocked loadouts
+    if (!model.gwoStartingCards) model.gwoStartingCards = [];
+    // IDs match the card filename minus file extension e.g. "gwc_start_vehicles"
+    model.gwoStartingCards.push(
       { id: "YOUR_LOADOUT_ID_1" },
       { id: "YOUR_LOADOUT_ID_2" },
       { id: "YOUR_LOADOUT_ID_N" }
