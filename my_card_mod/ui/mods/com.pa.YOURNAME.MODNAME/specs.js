4be36a0753d6ec3e9bb930594bfac53f59d49dd7
Quitch <Quitch@users.noreply.github.com>
2026-07-25 18:33:43 +0100

    Switch gwoSpecs example to use gwoUnits
    There's no reason to use paths

--- my_card_mod/ui/mods/com.pa.YOURNAME.MODNAME/specs.js
+++ my_card_mod/ui/mods/com.pa.YOURNAME.MODNAME/specs.js
@@ -1,12 +1,18 @@
 function addSpecs() {
   try {
-    if (!model.gwoSpecs) {
-      model.gwoSpecs = [];
-    }
-    // paths to files not used by the game that you wish to mod e.g. Ares' stomp
-    // example path: "pa/units/land/titan_vehicle/titan_vehicle_ammo_stomp.json"
-    // remove the line below if you don't want to mod unassigned specs
-    model.gwoSpecs.push("PATH_1", "PATH_2", "PATH_3");
+    requireGW(
+      ["coui://ui/mods/com.pa.quitch.gwaioverhaul/shared/units.js"],
+      // gwoUnit is unused until you uncomment the push below
+      // eslint-disable-next-line no-unused-vars
+      function (gwoUnit) {
+        if (!model.gwoSpecs) {
+          model.gwoSpecs = [];
+        }
+        // paths to files not used by the game that you wish to mod e.g. Ares' stomp
+        // uncomment the line below if you want to mod unassigned specs
+        // model.gwoSpecs.push(gwoUnit.aresStomp, gwoUnit.aresStompAmmo);
+      }
+    );
   } catch (e) {
     console.error(e);
     console.error(JSON.stringify(e));
